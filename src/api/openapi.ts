f579cb9908b2703344d254a962b8872899d9276a
zhangliang <2424123787@qq.com>
2025-02-07 22:57:32 +0800

    Update openapi.ts

--- src/api/openapi.ts
+++ src/api/openapi.ts
@@ -610,7 +610,7 @@ const getModelMax=( model:string )=>{
         return 16;
     }else if( model.indexOf('32k')>-1  ){
         return 32;
-    }else if( model.indexOf('gpt-4-turbo')>-1||  model.indexOf('gpt-4o')>-1 ||   model.indexOf('o1-')>-1){
+    }else if( model.indexOf('deep')>-1||model.indexOf('claude')>-1||model.indexOf('gpt-4-turbo')>-1||  model.indexOf('gpt-4o')>-1 ||   model.indexOf('o1-')>-1){
         return 128; 
     }else if( model.indexOf('64k')>-1  ){
         return 64;
